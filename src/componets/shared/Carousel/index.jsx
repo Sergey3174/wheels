ac031d222ec68ecb68423b274682d28e08ce8b4c
Alex.K <sobol4156@gmail.com>
2025-06-11 15:59:44 +0300

    fix: stop playing audio from iphone

--- src/componets/shared/Carousel/index.jsx
+++ src/componets/shared/Carousel/index.jsx
@@ -22,10 +22,17 @@ export const Carousel = () => {
   const repeatCount = useRef(3);
   const gap = useRef(8);
   const totalWidth = useRef(0);
+  const isIPhone = /iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-  // const startScrollSound = new Audio("/sounds/open-case.wav");
-  // const scrollSound = new Audio("/sounds/case-spin.mp3");
-  // const scrollEndSound = new Audio("/sounds/take-drop.wav");
+  const startScrollSound = new Audio("/sounds/open-case.wav");
+  const scrollSound = new Audio("/sounds/case-spin.mp3");
+  const scrollEndSound = new Audio("/sounds/take-drop.wav");
+
+  useEffect(() => {
+    startScrollSound.volume = 0.3;
+    scrollSound.volume = 0.3;
+    scrollEndSound.volume = 0.4;
+  }, []);
 
   // Получить предметы для рулетки
   const getItems = async () => {
@@ -80,11 +87,6 @@ export const Carousel = () => {
   };
 
   const createAnimation = (shift, winCard, prizeId) => {
-    // Импорт аудио
-    // startScrollSound.volume = 0.3;
-    // scrollSound.volume = 0.3;
-    // scrollEndSound.volume = 0.4;
-
     const randomOffset = getRandomShiftOffset();
 
     // Дополнительный отступ для различных экранов и отступов(чтобы нормально работала смена активного элемента)
@@ -112,10 +114,12 @@ export const Carousel = () => {
         willChange: "transform",
         ease: "power4.out",
         // звук открытия крутилки
-        // onStart: function () {
-        //   startScrollSound.currentTime = 0;
-        //   startScrollSound.play().catch(() => {});
-        // },
+        onStart: function () {
+          if (!isIPhone) {
+            startScrollSound.currentTime = 0;
+            startScrollSound.play().catch(() => {});
+          }
+        },
         onUpdate: function () {
           const currentX = gsap.getProperty(itemsBox.current, "x");
           let centerIndex;
@@ -134,16 +138,21 @@ export const Carousel = () => {
             //   .classList.remove("activeScroll");
           }
           // Для проигрыша звука
-          // if (currentActiveIndex.current !== centerIndex) {
-          //   scrollSound.currentTime = 0;
-          //   scrollSound.play().catch(() => {});
-          // }
+          if (currentActiveIndex.current !== centerIndex) {
+            if (!isIPhone) {
+              scrollSound.currentTime = 0;
+              scrollSound.play().catch(() => {});
+            }
+          }
           currentActiveIndex.current = centerIndex;
         },
         onComplete: () => {
           // проигрыш финального звука
-          // scrollEndSound.currentTime = 0;
-          // scrollEndSound.play().catch(() => {});
+          if (!isIPhone) {
+            scrollEndSound.currentTime = 0;
+            scrollEndSound.play().catch(() => {});
+          }
+
           const cards = document.querySelectorAll(".cardsScroll-item");
           // удаляем все возможные активные элементы
           cards.forEach((item) =>
